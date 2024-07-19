caadd9efbc83ff2dac167d7fdb6c8adf4a6010d6
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-07-18 23:37:22 -0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 'use client'
-
 import LayoutApp from "@/components/shared/LayoutApp/page";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default async function Component() {
-  const validar = () =>{
-    alert('Hello')
+export default function Component() {
+  
+  const Entrar = () =>  {
+
+    return  redirect("/cadastro")
   }
+
   return (
     <LayoutApp >
         <div className="flex flex-col items-center justify-center h-screen"> 
@@ -16,9 +19,15 @@ export default async function Component() {
 
             <div className="w-96 h-auto bg-blue-500 p-10 rounded-b-xl">
                 <form className="flex flex-col gap-3">
-                  <input className="outline-none p-3 rounded-md text-md text-black font-bold h-10" type="text" placeholder="E-mail" />
+                  <input 
+                    type="text" placeholder="E-mail" 
+                    className="outline-none p-3 rounded-md text-md text-black font-bold h-10" 
+                  />
                   <input className="outline-none p-3 rounded-md text-md text-black font-bold" type="password" placeholder="Senha" />
-                  <button className="p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700" onClick={validar}>Entrar </button>
+                  <button 
+                    onClick={Entrar}
+                    className="p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700" 
+                    >Entrar</button>
                   <Link 
                       href="/cadastro" 
                       className="p-3 rounded-md bg-slate-600 text-white hover:bg-slate-500 text-center"
