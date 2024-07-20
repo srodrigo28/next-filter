132e4feb9bcc383cfee6b754d20566b589284dde
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-07-20 12:12:25 -0300

    udated

--- app/(views)/cadastro/page.tsx
+++ app/(views)/cadastro/page.tsx
@@ -1,9 +1,12 @@
 import LayoutApp from "@/components/shared/LayoutApp/page";
-
+ 
 export default function Cadastro(){
     return(
         <LayoutApp>
             <h1>Cadastro</h1>
+
+            
+            
         </LayoutApp>
     )
 }
